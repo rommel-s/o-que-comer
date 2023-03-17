80528f7076d54acba9b6e57396a033708816a4ee
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-03-17 20:16:57 -0300

    Added manifest

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "/assets/logo/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
